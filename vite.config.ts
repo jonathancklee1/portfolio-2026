74f1934ff4543c3401e89b831494542d5ddeb089
Jonathan Lee <jonathancklee@gmail.com>
2026-06-02 20:36:27 +1000

    Fix config

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 import svgr from "vite-plugin-svgr";
 export default defineConfig({
-    base: "/portfolio-2026/",
+    base: "/",
     plugins: [
         react({
             babel: {
